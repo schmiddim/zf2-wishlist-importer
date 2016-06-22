a9b7c326e77dfccae90e48d29a0089a344293b25
ms <schmiddim@gmx.at>
2016-06-22 23:32:09 +0200

    usage

--- src/Schmiddim/Amazon/WishlistImporter/Module.php
+++ src/Schmiddim/Amazon/WishlistImporter/Module.php
@@ -10,8 +10,7 @@ use Zend\ModuleManager\Feature\BootstrapListenerInterface;
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
 use Zend\ModuleManager\Feature\ControllerPluginProviderInterface;
 use Zend\ModuleManager\Feature\ViewHelperProviderInterface;
-use Zend\Mvc\ApplicationInterface;
-
+use Zend\Console\Adapter\Posix;
 
 class Module implements
     AutoloaderProviderInterface,
@@ -69,5 +68,18 @@ class Module implements
     public function getConfig()
     {
         return include __DIR__ . '/../../../../config/module.config.php';
+    }
+
+    public function getConsoleUsage(Posix $console)
+    {
+        $routeItems = array();
+        $routeItems[] = 'List of all defined routes';
+        foreach ($this->getConfig()['console']['router']['routes'] as $route) {
+            $r = $route['options']['route'];
+            $action = $route['options']['defaults']['controller'] . ' ' . $route['options']['defaults']['action'] . 'Action';
+            $routeItems[] = [$r, $action];
+
+        }
+        return $routeItems;
     }
 }
